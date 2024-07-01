65343d73510e1ececcbca134714a682324be525a
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-07-01 08:17:11 +0200

    update: update icons for business info

--- src/features/ownerDashboard/BusinessInfor.js
+++ src/features/ownerDashboard/BusinessInfor.js
@@ -1,40 +1,43 @@
 import React from 'react';
 import { Typography, Box, Badge } from '@mui/material';
-import { People } from '@mui/icons-material';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+
+import AutoAwesomeTwoToneIcon from '@mui/icons-material/AutoAwesomeTwoTone';
+import NotificationsTwoToneIcon from '@mui/icons-material/NotificationsTwoTone';
+import InsertEmoticonTwoToneIcon from '@mui/icons-material/InsertEmoticonTwoTone';
 
 const BusinessInfo = ({ selectedBusiness, staff, appointments }) => {
-  return (
-    <Box textAlign="center" mb={4}>
-      <Box display="flex" justifyContent="center" alignItems="center" mb={2} position="relative">
-        <Typography variant="h5" gutterBottom className="business-title" flexGrow={1}>
-          {selectedBusiness.name}
-        </Typography>
-        <Box display="flex" alignItems="center" position="absolute" right={0}>
-          <Badge badgeContent={staff.length} color="primary" sx={{ marginRight: 2 }}>
-            <People className="material-symbols-outlined" />
-          </Badge>
-          <Badge badgeContent={appointments.length} color="error" sx={{ marginRight: 2 }}>
-            <NotificationsIcon className="material-symbols-outlined" />
-          </Badge>
-          {/* Commented out service section */}
-          {/* <Badge badgeContent={selectedBusiness.services?.length || 0} sx={{ "& .MuiBadge-badge": { backgroundColor: "green", color: "white" } }}>
-            <LocalOfferIcon className="material-symbols-outlined" />
-          </Badge> */}
+    return (
+        <Box textAlign="center" mb={4}>
+            <Box display="flex" justifyContent="center" alignItems="center" mb={2} position="relative">
+                <Typography variant="h5" gutterBottom className="business-title" flexGrow={1}>
+                    {selectedBusiness.name}
+                </Typography>
+                <Box display="flex" alignItems="center" position="absolute" right={0}>
+                    <Badge badgeContent={staff.length} color="primary" sx={{ marginRight: 2 }}>
+                        <InsertEmoticonTwoToneIcon className="material-symbols-outlined" />
+                    </Badge>
+                    <Badge badgeContent={appointments.length} color="error" sx={{ marginRight: 2 }}>
+                        <NotificationsTwoToneIcon className="material-symbols-outlined" />
+                    </Badge>
+
+                    <Badge badgeContent={selectedBusiness.services?.length || 0} sx={{ "& .MuiBadge-badge": { backgroundColor: "green", color: "white" } }}>
+                        <AutoAwesomeTwoToneIcon className="material-symbols-outlined" />
+                    </Badge>
+
+
+                </Box>
+            </Box>
+            <Typography variant="body1" gutterBottom className="business-detail">
+                <strong>Address:</strong> {selectedBusiness.address}
+            </Typography>
+            <Typography variant="body1" gutterBottom className="business-detail">
+                <strong>Phone:</strong> {selectedBusiness.phone}
+            </Typography>
+            <Typography variant="body1" gutterBottom className="business-detail">
+                <strong>Email:</strong> {selectedBusiness.email}
+            </Typography>
         </Box>
-      </Box>
-      <Typography variant="body1" gutterBottom className="business-detail">
-        <strong>Address:</strong> {selectedBusiness.address}
-      </Typography>
-      <Typography variant="body1" gutterBottom className="business-detail">
-        <strong>Phone:</strong> {selectedBusiness.phone}
-      </Typography>
-      <Typography variant="body1" gutterBottom className="business-detail">
-        <strong>Email:</strong> {selectedBusiness.email}
-      </Typography>
-    </Box>
-  );
+    );
 };
 
 export default BusinessInfo;
