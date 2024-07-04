35a6384dac865f154b44599a9b747bd374bcc9dd
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-07-04 09:45:27 +0200

    fix: fix authentication for business home + fix some path to apiClient

--- src/features/ownerDashboard/OwnerDashboard.js
+++ src/features/ownerDashboard/OwnerDashboard.js
@@ -5,7 +5,8 @@ import Footer from '../../components/Footer';
 import BusinessList from './BusinessList';
 import BusinessDetails from './BusinessDetails';
 import AppointmentList from '../appointment/AppointmentList';
-import { fetchBusinesses, fetchAppointments } from '../../lib/apiClient';
+import { fetchBusinesses} from '../../lib/apiClient';
+import { fetchAppointments } from '../../lib/apiClientAppointment';
 import '../../styles/css/OwnerDashboard.css';
 
 const OwnerDashboard = () => {
