a863fe6dc33645b0da6f006606e4476a84042dd6
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-08-21 09:14:40 +0200

    fix: fix time display in calendar + update time

--- src/features/calendar/CalendarEventContent.js
+++ src/features/calendar/CalendarEventContent.js
@@ -5,25 +5,26 @@ const CalendarEventContent = ({ eventInfo, currentView }) => {
 
   const startTime = eventInfo.event.start;
   const endTime = eventInfo.event.end;
-  const staffName = extendedProps.staffName;
+  //const staffName = extendedProps.staffName;
   const serviceName = extendedProps.service; // Access the service name from extendedProps
+  const customerName = extendedProps.customerName;
 
   if (!startTime || !endTime) {
     return <div><span>Invalid Time</span></div>;
   }
 
-  const timeText = `${startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - ${endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+  const timeText = `${startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}-${endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
 
   if (currentView === 'timeGridDay') {
     return (
       <div>
-        <span><strong>{timeText}</strong> {staffName}</span>
+        <span><strong>{timeText}</strong> {customerName}</span>
       </div>
     );
   } else if (currentView === 'resourceTimeGridDay') {
     return (
       <div>
-        <span><strong>{timeText}</strong> {staffName} - {serviceName}</span>
+        <span><strong>{timeText}</strong> {customerName} - {serviceName}</span>
       </div>
     );
   }
